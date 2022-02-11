f11afe950980a5da30d0c20354be5c867488cb58
Joeri Smissaert <smissaertj@gmail.com>
2022-02-11 11:07:25 +0400

    Get domain ID via get_domain_DNS(). Set Record Type in dot env.

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ SENDGRID_API_KEY = config('SENDGRID_API_KEY')
 CLOUDFLARE_API_KEY = config('CLOUDFLARE_API_KEY')
 CLOUFLARE_X_AUTH_EMAIL = config('CLOUFLARE_X_AUTH_EMAIL')
 CLOUDFLARE_ZONE_ID = config('CLOUDFLARE_ZONE_ID')
-CF_DOMAIN_ID = config('CF_DOMAIN_ID')
 CF_DOMAIN = config('CF_DOMAIN')
+DNS_RECORD_TYPE = config('DNS_RECORD_TYPE')
 FROM_EMAIL = config('FROM_EMAIL')
 TO_EMAIL = config('TO_EMAIL')
 
@@ -76,31 +76,32 @@ def get_public_ip():
 
 
 def get_domain_dns(domain):
-    """ Identify current A record value on Cloudflare """
+    """ Identify current record values on Cloudflare """
     try:
-        response = requests.get(CLOUDFLARE_API_BASE_URL + "/zones/" + CLOUDFLARE_ZONE_ID + "/dns_records/?type=A&name=" + domain, headers=cf_api_headers)
+        response = requests.get(CLOUDFLARE_API_BASE_URL + "/zones/" + CLOUDFLARE_ZONE_ID + "/dns_records/?type=" + DNS_RECORD_TYPE + "&name=" + domain, headers=cf_api_headers)
         json_data = json.loads(response.content)
     except Exception as error:
         logging.error(error)
     else:
         if response.status_code == 200:
-            return json_data["result"][0]["content"]
+            return json_data["result"][0]
 
 
-def update_domain_dns(public_ip):
+def update_domain_dns(public_ip, domain_id):
     """ Update the CF domain DNS A record with the value of public_ip """
 
-    data = {"type": "A", "name": CF_DOMAIN, "content": public_ip, "ttl": 3600, "proxied": True}
+    data = {"type": DNS_RECORD_TYPE, "name": CF_DOMAIN, "content": public_ip, "ttl": 3600, "proxied": True}
 
     try:
-        response = requests.put(CLOUDFLARE_API_BASE_URL + "/zones/" + CLOUDFLARE_ZONE_ID + "/dns_records/" + CF_DOMAIN_ID, headers=cf_api_headers, data=json.dumps(data))
+        response = requests.put(CLOUDFLARE_API_BASE_URL + "/zones/" + CLOUDFLARE_ZONE_ID + "/dns_records/" + domain_id, headers=cf_api_headers, data=json.dumps(data))
     except Exception as error:
         logging.error(error)
     else:
         if response.status_code == 200:
             send_notification(f"Domain DNS A Record updated: {public_ip}")
         else:
-            send_notification(f"Error updating domain DNS A record:\n{response.content}")
+            response = json.loads(response.content)
+            send_notification(f"Error updating domain DNS A record:\n{response}")
         return
 
 
@@ -111,8 +112,9 @@ def main():
     current_dns_value = get_domain_dns(CF_DOMAIN)
     logging.debug(f'Public IP: {public_ip} - Current DNS Value: {current_dns_value}')
 
-    if public_ip != current_dns_value:
-        update_domain_dns(public_ip)
+    if public_ip != current_dns_value['content']:
+        domain_id = current_dns_value['id']
+        update_domain_dns(public_ip, domain_id)
     else:
         logging.debug('No action needed')
 
